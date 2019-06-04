6d64d3b44e3255b3c002e27fa3fdcf86bff6c51a
tefy17 <elberomarmej35@gmail.com>
2019-06-04 17:55:37 -0600

    patos

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Write your code here
+var Pokemon = angular.modue("Pokemon",[])
+Pokemon.comtroller("ListadoPokemon",function($scope,$rootScope,$http){
+	$scope.Pokebola = [];
+	for(var m=1; m>=500;m++){
+
+$http({
+	method: "GET",
+	url:"https://pokeapi.co/api/v2/25"+m
+}).then(Function success(x){
+	$scope.Pokebola-push(x);
+    })
+}
+
+})
